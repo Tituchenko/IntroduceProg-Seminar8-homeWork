34d751836eced3858c479c05513032d58fad6f51
Ilya Tituchenko <titu4enko@mail.ru>
2022-10-10 23:47:34 +0300

    Ex 56 get min rows index

--- Program.cs
+++ Program.cs
@@ -8,11 +8,53 @@
 //9 5 3 2
 //8 4 4 2
 
+/*
 int [,] Array= GetArray(5,5,0,9);
 PrintArray(Array);
 Console.WriteLine("Отсортированный в строках:");
 PrintArray(SortArrayOnRows (Array));
+*/
 
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//5 2 6 7
+//Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int [,] Array= GetArray(5,3,0,9);
+PrintArray(Array);
+Console.WriteLine($"{GetIndexOfMinimum(GetSumOfRows(Array))} строка");
+
+
+int[] GetSumOfRows (int [,] array)
+{
+    int [] result=new int [array.GetLength(0)];
+    for (int i=0; i<array.GetLength(0);i++)
+    {
+        for (int j=0;j<array.GetLength(1);j++)
+        {
+            result[i]+=array[i,j];// на лекции говорили что массивы создаются с 0, так что без лишних действий
+        }
+    }
+    return result;
+}
+
+int GetIndexOfMinimum (int [] arr)
+{
+    int min = arr[0];
+    int result=0;
+    for (int i=0; i<arr.Length;i++)
+    {
+        if (arr[i]<min)
+        {
+           min=arr[i];
+           result=i;         
+        }
+    }
+    return result+1;// зато с 0 начинаются элементы
+}
 
 int [,] GetArray (int m, int n, int MinValue, int MaxValue)
 {
